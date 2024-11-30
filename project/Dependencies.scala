efe2a83c73565ac49b31fcb1b08ac804ffc33776
Eugene Yokota <eed3si9n@gmail.com>
2024-11-30 00:05:26 -0500

    Zinc 1.10.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,8 +14,8 @@ object Dependencies {
   // sbt modules
   private val ioVersion = nightlyVersion.getOrElse("1.10.2")
   private val lmVersion =
-    sys.props.get("sbt.build.lm.version").orElse(nightlyVersion).getOrElse("1.10.2")
-  val zincVersion = nightlyVersion.getOrElse("1.10.4")
+    sys.props.get("sbt.build.lm.version").orElse(nightlyVersion).getOrElse("1.10.3")
+  val zincVersion = nightlyVersion.getOrElse("1.10.5")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
